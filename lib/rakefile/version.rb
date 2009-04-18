26a9d7609f0bb2ca7d8012669700bb6c2025b958
Arto Bendiken <arto@bendiken.net>
2009-04-18 14:21:29 +0200

    Auto-generate a VERSION file containing '0.0.0' if one doesn't already exist.

--- lib/rakefile/version.rb
+++ lib/rakefile/version.rb
@@ -1,4 +1,8 @@
-desc "Displays the current version"
-task :version => 'VERSION' do
-  puts File.read('VERSION').chomp
+unless File.exists?('VERSION')
+  File.open('VERSION') { |f| f.puts "0.0.0" }
 end
+
+PROJECT_VERSION = File.read('VERSION').chomp
+
+desc "Displays the current version"
+task :version => 'VERSION' do puts PROJECT_VERSION end
